b340f602fcd56cbcc1ebb56ce56106205bd5d665
BalVil <balutavili@gmail.com>
2022-07-30 00:34:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter
+      basename={document.baseURI.substring(
+        document.baseURI.indexOf(window.location.origin) +
+          window.location.origin.length,
+        document.baseURI.lastIndexOf('/')
+      )}
+    >
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
